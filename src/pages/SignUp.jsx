5ef4bf9f043658b6f28740254dacb3fcce49b0a3
warrior-site <warhk69@gmail.com>
2026-02-14 22:53:38 +0530

    Update SignUp.jsx

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -27,7 +27,7 @@ function SignUp() {
 
 
 
-  };
+  
 
   return (
     <div className="flex items-center justify-center min-h-screen bg-gradient-to-br from-black via-gray-900 to-gray-800 text-white px-4">
